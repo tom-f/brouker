f937ed42ebcd4dcc86f2842cadae215461396cd5
Tom Fawssett <tom.fawssett@gmail.com>
2015-03-26 16:48:03 +0000

    Tie ie all together

--- brouker.go
+++ brouker.go
@@ -1,7 +1,38 @@
 package main
 
-import "fmt"
+import (
+	"flag"
+	"fmt"
+	"log"
+	"net/http"
+	"text/template"
+
+	"github.com/tom-f/brouker/broker"
+)
+
+var (
+	addr = flag.String("addr", ":8050", "http service address")
+)
+
+func homeHandler(c http.ResponseWriter, req *http.Request) {
+	tpl := template.Must(template.ParseFiles("./home.html"))
+	err := tpl.Execute(c, req.Host)
+
+	if err != nil {
+		fmt.Println(err)
+	}
+	// homeTempl = template.Must(template.ParseFiles(filepath.Join(*assets, "./home.html")))
+
+}
 
 func main() {
-  fmt.Println("Brouker!")
+	flag.Parse()
+	c := broker.NewCtrl()
+	go c.Run()
+	http.HandleFunc("/", homeHandler)
+	http.Handle("/ws", broker.ConnHandler{C: c})
+	http.Handle("/msg", broker.CtrlWriter{C: c})
+	if err := http.ListenAndServe(*addr, nil); err != nil {
+		log.Fatal("ListenAndServe:", err)
+	}
 }
